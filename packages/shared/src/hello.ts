5139fd263965ebf51a53c0ac22b802dd918a7fe4
liweihong <liwhong@yonyou.com>
2023-12-19 14:16:06 +0800

    feat:add ts's *.d.ts command and code

--- packages/shared/src/hello.ts
+++ packages/shared/src/hello.ts
@@ -1,6 +1,9 @@
 /**
- * hello测试方法
- * @param to
+ * [hello description]
+ * @param    {number = 'World'} to [description]
+ * @author   codefarmer
+ * @version  [1.0.0]
+ * @datetime 2023-12-19T09:25:41+0800
  */
 export function hello(to: string = 'World') {
     const txt = `Hello ${to}!`;
